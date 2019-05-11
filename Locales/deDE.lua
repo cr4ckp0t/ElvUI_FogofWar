23abc74ba5760d257a2900a6f2abdf035aaf18e1
Adam Koch <me@adamko.ch>
2019-05-11 15:48:35 -0400

    Merged fixes from ElvUI Fog Remover by Kilx. Kilx's code used with permission.

---
+++ Locales/deDE.lua
@@ -0,0 +1,15 @@
+-------------------------------------------------------------------------------
+-- ElvUI Fog of War By Lockslap (US, Bleeding Hollow)
+-------------------------------------------------------------------------------
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ElvUI_FogofWar", "deDE", false)
+if not L then return end
+
+L["Fog of War"] = "Fog of War" -- Requires localization
+L["Overlay Color"] = "Overlay Color" -- Requires localization
+L["Plugins by |cff9382c9Lockslap|r"] = "Plugins by |cff9382c9Lockslap|r" -- Requires localization
+L["Thanks for using and supporting my work!  -- |cff9382c9Lockslap|r\
+\
+|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."] = "Thanks for using and supporting my work!  -- |cff9382c9Lockslap|r\
+\
+|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge." -- Requires localization
---
+++ Locales/enUS.lua
@@ -0,0 +1,15 @@
+-------------------------------------------------------------------------------
+-- ElvUI_FogofWar By Lockslap (US, Bleeding Hollow)
+-------------------------------------------------------------------------------
+local debug = false
+--[===[@debug@
+debug = true
+--@end-debug@]===]
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ElvUI_FogofWar", "enUS", true, debug)
+if not L then return end
+
+L["Fog of War"] = true
+L["Overlay Color"] = true
+L["Plugins by |cff9382c9Lockslap|r"] = true
+L["Thanks for using and supporting my work!  -- |cff9382c9Lockslap|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."] = true
