305d2bae99503b4148caee4763f45e9c6ba1c2cc
Adam Koch <me@adamko.ch>
2020-11-22 11:07:42 -0500

    Added support for Shadowlands (thanks Sugarfoot!) Fixed localization. Updated TOC for 9.0.2.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- ElvUI_FogofWar By Crackpotx (US, Lightbringer)
+-- ElvUI_FogofWar By Crackpotx (US, Arthas)
 -------------------------------------------------------------------------------
 local debug = false
 --@debug@
@@ -9,7 +9,4 @@ local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI_FogofWar", "enUS", true, debug)
 if not L then return end
 
-L["Fog of War"] = true
-L["Overlay Color"] = true
-L["Plugins by |cff9382c9Crackpotx|r"] = true
-L["Thanks for using and supporting my work!  -- |cff9382c9Crackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."] = true
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
