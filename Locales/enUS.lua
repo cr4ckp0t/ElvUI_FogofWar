28f4595c89578ab1c197de7d6cd219aac2a7ef46
crborga <chrisborga@yahoo.com>
2020-12-01 22:16:38 -0500

    Update enUS.lua

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -9,4 +9,7 @@ local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI_FogofWar", "enUS", true, debug)
 if not L then return end
 
---@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+L["Fog of War"] = true
+L["Overlay Color"] = true
+L["Plugins by |cff9382c9Crackpotx|r"] = true
+L["Thanks for using and supporting my work!  -- |cff9382c9Crackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."] = true
