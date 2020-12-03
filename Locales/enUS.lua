93cb8999515604bc9523928b4576bc342582d7a4
crborga <chrisborga@yahoo.com>
2020-12-03 14:35:16 -0500

    Update enUS.lua

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,5 +1,6 @@
--- ElvUI Fog of War By Crackpotx (US, Arthas)
 -------------------------------------------------------------------------------
+-- ElvUI Fog of War By Crackpotx (US, Arthas)
+--------------------------------------------------------------------------------
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI_FogofWar", "enUS", false)
 if not L then return end
